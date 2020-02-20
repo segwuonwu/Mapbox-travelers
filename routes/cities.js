097ce0352acb7afe24aa7b628ff56d09908394c2
segwuonwu <segwuonwu@gmail.com>
2020-02-20 11:03:41 -0800

    Geocoding works upon submit

--- routes/cities.js
+++ routes/cities.js
@@ -7,13 +7,25 @@ const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_API_KEY }
 router.get('/results', (req, res) => {
     geocodingClient.forwardGeocode({ query: req.query.name + ' ' + req.query.state })
         .send().then(response => {
-            res.send(response.body);
+            let places = response.body.features.filter(result => {
+                if (result['place_type'][0] === 'place') {
+                    return true;
+                }
+            }).map(city => {
+                return {
+                    name: city['place_name'].split(',')[0],
+                    state: city['place_name'].split(',')[1],
+                    lat: city.center[1],
+                    long: city.center[0]
+                }
+            });
+            res.send(places);
         })
 
     //res.send('LOOK FOR A CITY');
 })
 
-router.get('/test', (req, res) => {
+/*router.get('/test', (req, res) => {
     let palces;
     geocodingClient.forwardGeocode({
         query: 'Seattle, WA'
@@ -33,6 +45,6 @@ router.get('/test', (req, res) => {
         });
         res.send(places);
     })
-})
+})*/
 
 module.exports = router;
