4045cf7774c63d0829d357c7ad32b3df82b53d60
segwuonwu <segwuonwu@gmail.com>
2020-02-20 10:47:59 -0800

    set up the home view

---
+++ routes/cities.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_API_KEY });
+
+router.get('/results', (req, res) => {
+    geocodingClient.forwardGeocode({ query: req.query.name + ' ' + req.query.state })
+        .send().then(response => {
+            res.send(response.body);
+        })
+
+    //res.send('LOOK FOR A CITY');
+})
+
+router.get('/test', (req, res) => {
+    let palces;
+    geocodingClient.forwardGeocode({
+        query: 'Seattle, WA'
+    }).send().then(response => {
+        //send array of only place
+        places = response.body.features.filter(result => {
+            if (result['place_type'][0] === 'place') {
+                return true;
+            }
+        }).map(city => {
+            return {
+                name: city['place_name'].split(',')[0],
+                state: city['place_name'].split(',')[1],
+                lat: city.center[1],
+                long: city.center[0]
+            }
+        });
+        res.send(places);
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-let express = require('express')
-let app = express()
-let ejsLayouts = require('express-ejs-layouts')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const ejsLayouts = require('express-ejs-layouts');
 
 app.use(ejsLayouts);
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("static"));
 
-app.get('/', (req, res)=>{
-  res.render('home')
-})
+app.get('/', (req, res) => {
+    res.render('home')
+});
 
-app.listen(process.env.PORT || 8000, console.log('🎧 Port 8000 🎧'))
+app.use('/', require('./routes/cities'));
+
+app.get('/*', (req, res) => {
+    res.render('404');
+});
+
+app.listen(process.env.PORT || 8000, console.log(`🎧🍵 You're listening to the smooth sounds of port ${process.env.PORT || 8000} 🎧`));
